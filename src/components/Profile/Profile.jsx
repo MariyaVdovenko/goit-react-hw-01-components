bb801877045f8b7d28403daba4491b1d2310b54a
MariyaVdovenko <m.vdovenko@ukr.net>
2019-11-30 01:36:54 +0200

    add destructuring to profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,36 +2,39 @@ import React from 'react';
 import T from 'prop-types';
 import styles from './Profile.module.css';
 
-const Profile = ({
-  name,
-  tag,
-  location,
-  avatar,
-  stats: { followers: fl, views: vie, likes: lk },
-}) => (
-  <div className={styles.Profile}>
-    <div className="description">
-      <img src={avatar} alt="user avatar" className={styles.avatar} />
-      <p className={styles.name}>{name}</p>
-      <p className={styles.tag}>{tag}</p>
-      <p className={styles.location}>{location}</p>
-      <ul className={styles.stats}>
-        <li className={styles.statsItem}>
-          <span className={styles.label}>Followers </span>
-          <span className={styles.quantity}>{fl}</span>
-        </li>
-        <li className={styles.statsItem}>
-          <span className={styles.label}>Views </span>
-          <span className={styles.quantity}>{vie}</span>
-        </li>
-        <li className={styles.statsItem}>
-          <span className={styles.label}>Likes </span>
-          <span className={styles.quantity}>{lk}</span>
-        </li>
-      </ul>
+const Profile = ({ user }) => {
+  const {
+    avatar,
+    name,
+    tag,
+    location,
+    stats: { followers, views, likes },
+  } = user;
+  return (
+    <div className={styles.Profile}>
+      <div className="description">
+        <img src={avatar} alt="user avatar" className={styles.avatar} />
+        <p className={name}>{user.name}</p>
+        <p className={tag}>{user.tag}</p>
+        <p className={location}>{user.location}</p>
+        <ul className={styles.stats}>
+          <li className={styles.statsItem}>
+            <span className={styles.label}>Followers </span>
+            <span className={styles.quantity}>{followers}</span>
+          </li>
+          <li className={styles.statsItem}>
+            <span className={styles.label}>Views </span>
+            <span className={styles.quantity}>{views}</span>
+          </li>
+          <li className={styles.statsItem}>
+            <span className={styles.label}>Likes </span>
+            <span className={styles.quantity}>{likes}</span>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Profile.propTypes = {
   user: T.shape({
